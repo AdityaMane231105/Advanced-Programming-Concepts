cf0c550a11b1d98b13d3714b6171e5ef45dc87fd
AdityaMane231105 <adityamane104798@gmail.com>
2026-08-20 10:00:19 +0530

    Update URL extraction example

--- Web Scraper-Extract  URLs.py
+++ Web Scraper-Extract  URLs.py
@@ -1,10 +1,19 @@
 import re
 
-def extract_urls(html_content):
+def extract_urls(text):
     pattern = r'(https?://[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}[^\s"<]*)|(www\.[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}[^\s"<]*)'
-    urls = re.findall(pattern, html_content)
+    urls = re.findall(pattern, text)
     return [url for tup in urls for url in tup if url]
 
-html = '<a href="https://example.com">Link</a> Visit www.test.org now!'
-print(extract_urls(html))
+paragraphs = """
+I was reading an article yesterday and found a useful resource at https://docs.python.org/3/tutorial/.
+You can also check out www.learnprogramming.com for beginner-friendly guides.
+
+Later, I visited https://github.com to explore open-source projects.
+Another great site is www.stackoverflow.com where developers ask and answer coding questions.
+
+For UI/UX design inspiration, I often browse https://dribbble.com and www.behance.net.
+"""
+
+print(extract_urls(paragraphs))
 
